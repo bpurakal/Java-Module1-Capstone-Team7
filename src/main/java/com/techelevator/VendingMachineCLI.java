95bb107dfc2abe8d51a6f775d113a988e346cd11
Eran KaranouhSchuler <ekaranou@kent.edu>
2018-02-09 21:35:28 -0500

    FINISHEDgit add .git add .git add .git add .git add .git add .git add .git add .

--- src/main/java/com/techelevator/VendingMachineCLI.java
+++ src/main/java/com/techelevator/VendingMachineCLI.java
@@ -1,6 +1,7 @@
 package com.techelevator;
 
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.math.BigDecimal;
 import java.util.Scanner;
 
@@ -28,9 +29,13 @@ public class VendingMachineCLI {
 		this.menu = menu;
 	}
 
-	public void run() throws FileNotFoundException {
+	public void run() throws IOException {
 		VendingMachine VM500 = new VendingMachine();
 		PurchaseBasket yourCart = new PurchaseBasket();
+		LogTheLogger logFile = new LogTheLogger();
+		
+		logFile.logMethod("Initialize", new BigDecimal("0"), new BigDecimal("0"));
+		
 		while (true) {
 			String choice = (String) menu.getChoiceFromOptions(MAIN_MENU_OPTIONS);
 
@@ -47,6 +52,7 @@ public class VendingMachineCLI {
 							+ VM500.getInventory().get(slotID).toArray()[0] + " \nAmount Left "
 							+ VM500.getInventory().get(slotID).size());
 					counter++;
+					
 				}
 			} else if (choice.equals(MAIN_MENU_OPTION_PURCHASE)) {
 				// do purchase
@@ -58,6 +64,9 @@ public class VendingMachineCLI {
 						while (amount != null) {
 							VM500.addToBalance(amount);
 							System.out.println("Your current balance is: $" + VM500.getBalance());
+							
+							logFile.logMethod("FEED MONEY", amount, VM500.getBalance());
+							
 							amount = menu.getAmountFromUserInput();
 						}
 					}
@@ -68,9 +77,15 @@ public class VendingMachineCLI {
 						Item boughtItem = null;
 						if(VM500.getInventory().containsKey(purchaseKey)) {
 							try {
+								
+								BigDecimal tempBalance = VM500.getBalance();
+								
 								boughtItem = VM500.purchaseItem(purchaseKey);
 								yourCart.addToBasket(boughtItem);
 								System.out.println(boughtItem);
+								
+								logFile.logMethod(boughtItem.getName(), tempBalance, VM500.getBalance());
+								
 							} catch (OutOfStockException e) {
 								System.out.println(e.getMessage());
 								choice.equals(PURCHASE_DISPLAY_SELECT);
@@ -87,7 +102,13 @@ public class VendingMachineCLI {
 					}
 
 					if (choice.equals(PURCHASE_DISPLAY_FINAL)) {
+						
+						BigDecimal tempBalance = VM500.getBalance();
+						
 						System.out.println(VM500.returnChange());
+						
+						logFile.logMethod("GIVE CHANGE", tempBalance, VM500.getBalance());
+						
 						System.out.println(yourCart.consumeBasket());
 						break;
 					}
@@ -99,7 +120,7 @@ public class VendingMachineCLI {
 
 	}
 
-	public static void main(String[] args) throws FileNotFoundException {
+	public static void main(String[] args) throws IOException {
 		Menu menu = new Menu(System.in, System.out);
 		VendingMachineCLI cli = new VendingMachineCLI(menu);
 		cli.run();
